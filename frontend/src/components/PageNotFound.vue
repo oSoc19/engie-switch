2cac486405dda920a66b6d6a549e1b119f68a670
Axtux <axtux@hotmail.com>
2019-07-02 16:31:02 +0200

    Vue.js

---
+++ frontend/src/components/PageNotFound.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">
+    <h1>Erreur 404</h1>
+    <p>
+      Page not found.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PageNotFound'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
