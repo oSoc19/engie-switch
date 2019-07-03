ba40d6cf36a378e3afbfab6dca050fa0140cecdd
aswevers <amber.swevers@hotmail.com>
2019-07-03 16:25:31 +0200

    Same as before

--- hackathon/hackathon_frontend/src/App.vue
+++ hackathon/hackathon_frontend/src/App.vue
@@ -1,19 +1,13 @@
 <template>
   <div id="app">
-<<<<<<< HEAD
-    
-    <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
-    <home msg="Challenge of the day"/>
-=======
     <router-view></router-view>
->>>>>>> 4fe29454f10d12d93c7c251635a2b3a16f76b9cb
   </div>
 </template>
 
 <script>
 
 export default {
-  name: 'App'
+  name: 'Appgit'
 }
 </script>
 
