35e110c2b30018cd34590e73dc44115460c2965b
nawatend <32384185+nawatend@users.noreply.github.com>
2019-07-04 09:33:47 +0200

    daily card challenge basic setup

--- hackathon/hackathon_frontend/src/main.js
+++ hackathon/hackathon_frontend/src/main.js
@@ -4,16 +4,20 @@ import App from './App.vue'
 
 import HelloWorld from '@/components/HelloWorld'
 import HelloNobody from '@/components/HelloNobody'
+import DailyChallengeCard from '@/components/DailyChallengeCard'
 
 // rename component
 Vue.component('hello-world', HelloWorld)
 Vue.component('hello-nobody', HelloNobody)
+Vue.component('daily-challenge-card', DailyChallengeCard)
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   // first component:
-  components: { App },
+  components: {
+    App
+  },
   template: '<App/>'
 }).$mount('#app')
