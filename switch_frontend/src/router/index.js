358771447661f6e44dc25eef32f87c981b079b47
aswevers <amber.swevers@hotmail.com>
2019-07-09 09:28:48 +0200

    Homepage and rename Home.vue

--- switch_frontend/src/router/index.js
+++ switch_frontend/src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
-import Home from "@/views/HelloWorld.vue";
+import Home from "@/views/Home.vue";
 import Next from "@/views/Next.vue";
 
 Vue.use(VueRouter)
--- switch_frontend/src/views/Home.vue
+++ switch_frontend/src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <card />
-    
+    <navigation />
   </div>
 </template>
 
