266450d9550766ed1cc7087dcacdb49793400442
Andrews Leruth <andrewsleruth@MacBook-Pro-de-thierry.local>
2019-07-09 11:40:50 +0200

    add models as required in index.js

---
+++ switch_backend/src/models/index.js
@@ -0,0 +1,7 @@
+// This file is here to require all of the files in this directory
+// That way we just load this file instead of having to require
+// so many things in our app.js
+
+require('./Challenge');
+require('./User');
+require('./Post');
