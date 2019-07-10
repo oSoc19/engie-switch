bf517acc4490e22a66716763166f76db5ee3b396
Andrews Leruth <andrewsleruth@MacBook-Pro-de-thierry.local>
2019-07-10 13:48:31 +0200

    update the route to find a user by id

--- switch_backend/src/routes/index.js
+++ switch_backend/src/routes/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const createError = require('http-errors');
 
 //These are the models we'll be interacting with
 let challenge = mongoose.model('Challenge');
 let user = mongoose.model('User');
 let post = mongoose.model('Post');
+
+// Here we are using promises to have only one error handler
+// when doing mongoose queries
 
 // Check if the server is running
 router.get('/', (req, res, next) => {
@@ -14,19 +18,33 @@ router.get('/', (req, res, next) => {
 
 // Get a user by it's id
 router.get('/users/:id', (req, res, next) => {
-  user.findOne({"id": req.params.id}, (err, user) => {
-    if(err) return next(err);
-    if(!user) return next(new Error('Not found ' + req.params.id));
-    res.json(user);
+  user.findById(req.params.id).exec()
+  .then((userFound) => {
+    if (!userFound) throw new createError(404,"Couldn't find user " + req.params.id);
+    res.json(userFound);
+  })
+  .catch((err) => {
+    console.log(err);
+    next(err);
   });
 });
 
-// Here we are using promises to have only one error handler
+// Get all users (TESTING PURPOSES ONLY)
+router.get('/users/', (req, res, next) => {
+  user.find({}, (err, users) => {
+    if(err) return next(err);
+    res.json(users);
+  });
+});
+
+// Create a new user
+// users can't have the same username
 router.post('/users/', (req, res, next) => {
   let tempUser = new user(req.body);
   user.findOne({username: tempUser.username}).exec()
   .then((found) => {
-    if(found) throw new Error('User already exists!');
+    console.log(found);
+    if(found) throw new createError(400, 'User already exists!');
     else return tempUser.save();
   })
   .then((savedUser) => {
