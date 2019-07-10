200b38e9b2074b90057f343b48a93885cbe72414
aswevers <amber.swevers@hotmail.com>
2019-07-10 10:42:45 +0200

    Challenge card - lay-out finished

---
+++ switch_frontend/src/components/ChallengeCard.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="challenge">
+        <div class="challenge__content">
+            <div class="challenge__content__image"/>
+            <div class="challenge__content__text">
+                <p class="challenge__content__text__title">Fill your fridge</p>
+                <p class="challenge__content__text__description">By filling your fridge you will have less cold air escape every time you open it.</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ChallengeCard"
+}
+</script>
+
+<style>
+.challenge{
+    display: flex;
+    justify-content: center;
+}
+
+.challenge__content{
+    display: flex;
+    width: 88%;
+    box-shadow: 0px 7px 12px -3px rgba(158,158,158,1);
+    border-radius: 8px;
+    height: 110px;
+}
+
+.challenge__content__image{
+    margin: 10px;
+    width: 36%;
+    background-image: url("../assets/img/fridge.jpg");
+    background-size: cover;
+    background-position: center;
+    border-radius: 5px;
+}
+
+.challenge__content__text{
+    width: 64%;
+    height: 100px;
+    display: flex;
+    flex-direction: column;
+}
+
+.challenge__content__text__title{
+    color: #444444;
+    font-weight: bolder;
+    text-align: left;
+    margin-bottom: 3px;
+    margin-top: 10px;
+}
+
+.challenge__content__text__description{
+    font-size: 14px;
+    color: #444444;
+    text-align: left;
+    margin-top: 0px;
+}
+</style>
--- switch_frontend/src/views/Challenges.vue
+++ switch_frontend/src/views/Challenges.vue
@@ -1,8 +1,7 @@
 <template>
   <div class="hello">
     <h1>Challenges</h1>
-    <card />
-    <navigation />
+    <challengecard />
   </div>
 </template>
 
