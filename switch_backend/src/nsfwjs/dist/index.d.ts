a0dcc82e6bc27ad21f53b7aa19113596d8d5514e
Axtux <axtux@hotmail.com>
2019-07-15 18:29:56 +0200

    add nsfwjs in the backend

---
+++ switch_backend/src/nsfwjs/dist/index.d.ts
@@ -0,0 +1,38 @@
+import * as tf from '@tensorflow/tfjs';
+import * as SuperGif from 'libgif';
+interface frameResult {
+    index: number;
+    totalFrames: number;
+    predictions: Array<Object>;
+}
+interface classifyConfig {
+    topk?: number;
+    onFrame?: (result: frameResult) => {};
+    setGifControl?: (gifControl: typeof SuperGif) => {};
+}
+interface nsfwjsOptions {
+    size: number;
+}
+export declare function load(base?: string, options?: {
+    size: number;
+}): Promise<NSFWJS>;
+export declare class NSFWJS {
+    endpoints: string[];
+    private options;
+    private path;
+    private model;
+    private intermediateModels;
+    private normalizationOffset;
+    constructor(base: string, options: nsfwjsOptions);
+    load(): Promise<void>;
+    infer(img: tf.Tensor3D | ImageData | HTMLImageElement | HTMLCanvasElement | HTMLVideoElement, endpoint?: string): tf.Tensor;
+    classify(img: tf.Tensor3D | ImageData | HTMLImageElement | HTMLCanvasElement | HTMLVideoElement, topk?: number): Promise<Array<{
+        className: string;
+        probability: number;
+    }>>;
+    classifyGif(gif: HTMLImageElement, config?: classifyConfig): Promise<Array<Array<{
+        className: string;
+        probability: number;
+    }>>>;
+}
+export {};
