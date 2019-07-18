006fef6129353c1e3a7414d2d908131109a232ae
unknown <andrewsleruth@gmail.com>
2019-07-18 14:25:20 +0200

    update login so that it returns user update top 10 to return id

--- switch_backend/src/routes/users/index.js
+++ switch_backend/src/routes/users/index.js
@@ -17,23 +17,42 @@ module.exports = router
 
   // Get top 10
   //TODO get this route working
-  .get('/top10users', (req, res, next) => {
-    user.find({}, (err, users) => {
+  .get('/top10users', checkToken, (req, res, next) => {
+    console.log('start of top 10')
+    let decoded = req.decoded;
+    console.log('after req.decoded')
+    user.find({}).exec()
+    .then((users) => {
+      console.log('before sortedUsers')
+      let sortedUsers = users.sort(sortUsersByPoints);
 
-      if (err) return next(err);
-      let sortedUsers = users.sort(sortUsersByPoints)
-      console.log(sortedUsers[0].points)
-      console.log(sortedUsers[1].points)
-      res.json(sortedUsers.slice(0, 10))
-    });
+      console.log(sortedUsers[0].points);
+      console.log(sortedUsers[1].points);
+
+      user.findById(decoded.id).exec()
+      .then((userFound) => {
+        if (!userFound) throw new createError(404, "Couldn't find user " + decoded.id);
+        else res.json({
+          "top10" : sortedUsers.slice(0, 10),
+          "user" : userFound
+        });
+      })
+    }).catch(err => {
+      console.log(err);
+      next(err);
+    })
   })
 
   // login user using the token
-  .get('/login', checkToken, (req, res) => {
+  .get('/login', checkToken, (req, res, next) => {
 
     let decoded = req.decoded;
-    res.json({
-      "id": decoded.id
+    user.findById(decoded.id).exec()
+    .then((userFound) => {
+      if (!userFound) throw new createError(404, "Couldn't find user " + decoded.id);
+      else res.json(userFound);
+    }).catch(err => {
+      next(err);
     });
   })
   // Get a user by it's id
@@ -42,8 +61,7 @@ module.exports = router
       .then((userFound) => {
         if (!userFound) throw new createError(404, "Couldn't find user " + req.params.id);
         else res.json(userFound);
-      })
-      .catch((err) => {
+      }).catch((err) => {
         console.log(err);
         next(err);
       });
@@ -53,9 +71,7 @@ module.exports = router
   // If it does, send an error and retry to do it
   .put('/:id', (req, res, next) => {
 
-    let userToFind = {
-      username: req.body.username
-    }
+    let userToFind = {username: req.body.username};
 
     user.findOne(userToFind).exec()
       .then((userFound) => {
@@ -77,9 +93,11 @@ module.exports = router
   })
   // Get all users (TESTING PURPOSES ONLY)
   .get('/', (req, res, next) => {
-    user.find({}, (err, users) => {
-      if (err) return next(err);
+    user.find({}).exec()
+    .then((users) => {
       res.json(users);
+    }).catch(err => {
+      next(err);
     });
   })
 
@@ -97,9 +115,7 @@ module.exports = router
   // TODO: generate random username
   .post('/', (req, res, next) => {
     let tempUser = new user(req.body);
-    user.findOne({
-        username: tempUser.username
-      }).exec()
+    user.findOne({username: tempUser.username}).exec()
       .then((found) => {
         console.log(found);
         if (found) throw new createError(400, 'User already exists!');
