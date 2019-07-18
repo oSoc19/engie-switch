b9b2c63dfe0c2d6f0b4b7692360ea05d36f3f67f
Axtux <axtux@hotmail.com>
2019-07-18 15:50:43 +0200

    move api stuff in API modul

--- switch_frontend/src/App.vue
+++ switch_frontend/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <topbar v-if="userData" v-bind:userData="userData"></topbar>
+    <topbar v-if="user" v-bind:userData="user"></topbar>
     <router-view></router-view>
     <navigation class="navigation" />
   </div>
@@ -9,60 +9,17 @@
 <script>
 window.$ = require("jquery");
 window.JQuery = require("jquery");
-import { postRequest, getCurrentUserData } from "./utils";
+import api from '@/utils/api'
+
 export default {
   name: "app",
   data() {
     return {
-      userData: {}
+      user: {}
     };
   },
-  async created() {
-    //check if there is token in localstorage+
-
-    if (localStorage.getItem("switch_token") === null) {
-      //register new user with random Name if no token in localstorage
-      let randomNr = Math.floor(Math.random() * 1000000000);
-      let newUser = {
-        username: "Anonymous " + randomNr,
-        profilePic: "BASE NNNNNNNNNNNNNNnn",
-        points: 0
-      };
-
-      let newUserData = await postRequest(
-        "http://localhost:3000/users",
-        newUser
-      );
-      // console.log(newUserData);
-
-      localStorage.setItem("switch_token", newUserData.token);
-
-      //get that new user data
-      let userDataAll = await getCurrentUserData(
-        "http://localhost:3000/users/login",
-        localStorage.getItem("switch_token")
-      );
-
-      this.userData = {
-        username: userDataAll.username,
-        points: userDataAll.points,
-        profilePic: userDataAll.profilePic
-      };
-    } else {
-      //get token and get userData
-      let userDataAll = await getCurrentUserData(
-        "http://localhost:3000/users/login",
-        localStorage.getItem("switch_token")
-      );
-
-      this.userData = {
-        username: userDataAll.username,
-        points: userDataAll.points,
-        profilePic: userDataAll.profilePic
-      };
-
-      //console.log(this.userData);
-    }
+  created() {
+    api.initAuth();
   }
 };
 </script>
--- switch_frontend/src/utils/api.js
+++ switch_frontend/src/utils/api.js
@@ -1,14 +1,53 @@
 const BASE_URL = 'http://localhost:3000'
+let user = null;
+
+
+/*
+ * Local functions
+ */
+function createUser() {
+  let randomNr = Math.floor(Math.random() * 1e6);
+  user = {
+    username: "Anonymous " + randomNr,
+    profilePic: '',
+    points: 0
+  };
+
+  return apiCall('/users', user).then((data) => {
+    localStorage.setItem("switch_token", data.token);
+    user = data
+  });
+}
+
+function fetchUser() {
+  return apiCall('/users/login', null, true);
+}
 
 function apiCall(path, data=null, auth=false) {
-  if(auth) {
-    // TODO add Authorization header
-  }
   return new Promise((resolve, reject) => {
-    window.$.getJSON(BASE_URL+path, data)
-      .done(resolve)
-      .fail(reject);
+    window.$.ajax(BASE_URL+path, {
+      data : data ? JSON.stringify(data) : null,
+      contentType : data ? 'application/json' : null,
+      type : data ? 'POST' : 'GET',
+      headers: auth ? {'Authorization': localStorage.getItem("switch_token")} : {},
+    })
+    .done(resolve)
+    .fail(reject);
   })
+}
+
+/*
+ * External functions
+ */
+function initAuth() {
+  if(user) {
+    return Promise.resolve(user);
+  }
+  if(localStorage.getItem("switch_token")) {
+    return fetchUser()
+  } else {
+    return createUser()
+  }
 }
 
 function getPosts() {
@@ -28,6 +67,7 @@ function getUser(id) {
 }
 
 export default {
+  initAuth,
   getPosts,
   getChallenges,
   getUsers,
