1128784308e045dbe74a0190468461bb94f64286
Axtux <axtux@hotmail.com>
2019-07-20 19:08:09 +0200

    use API

--- switch_frontend/src/views/UploadPost.vue
+++ switch_frontend/src/views/UploadPost.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="wrapper">
-    <upload-image />
-    <div class="actions">
-      <div id="btnPost">POST</div>
-    </div>
-
-    <div class="challenge__detail">
-      <div class="challenge__title">{{this.challengeId}}</div>
-      <div class="challenge__image"></div>
-      <div class="challenge__description"></div>
-    </div>
+    <ion-card v-if="challenge">
+      <img :src="challenge.image" />
+      <ion-card-header>
+        <!--ion-card-subtitle>{{ challenge.title }}</ion-card-subtitle-->
+        <ion-card-title>{{ challenge.title }}</ion-card-title>
+      </ion-card-header>
+      <ion-card-content>{{ challenge.description }}</ion-card-content>
+    </ion-card>
+    <upload-image :challengeId="challengeId"/>
+    <p>Upload your picture of the challenge</p>
   </div>
 </template>
 
 <script>
-import { postRequest, getRequest } from "../utils";
+import api from '@/utils/api'
 
 export default {
   name: "UploadPost",
@@ -23,61 +23,25 @@ export default {
   },
   methods: {
     getChallengeById: function() {
-      let challenge = getRequest(
-        "http://localhost:3000/challenges/" + this.$route.params.challengeId
-      );
-      this.challenge = challenge;
-    }
+      api.getChallenge(this.challengeId).then(challenge => this.challenge = challenge);
+    },
   },
   data() {
     return {
-      userId: "5d25cb9464ac96e8c233f474",
       challengeId: this.$route.params.challengeId,
-      challenge: "",
+      challenge: {},
       postAble: true
     };
   },
   mounted() {
-    //set data
-    this.challengeId = this.$route.params.challengeId;
-    //get token from localstorage
-
-    let btnPost = document.getElementById("btnPost");
-
-    btnPost.addEventListener("click", e => {
-      e.preventDefault();
-
-      let imageBase64 = localStorage.getItem("imagePost");
-
-      if (this.postAble) {
-        //new post object
-        let newPost = {
-          challenge: this.challengeId,
-          user: this.userId,
-          image: imageBase64, //BASE64_CONTENT
-          text: "This text is very nice",
-          reviews: 0
-        };
-
-        postRequest("http://localhost:3000/posts", newPost, "post").then(() => {
-          this.postAble = false;
-          //redirect to home
-          this.$router.push("/");
-        });
-      } else {
-        alert("Use a good image!");
-      }
-    });
+    // fetch challenge data
+    this.getChallengeById();
   }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-@import "../css/variables.css";
-.test__img {
-  max-width: 100%;
-}
 
 .wrapper {
   width: 100%;
@@ -86,24 +50,5 @@ export default {
   align-items: center;
   flex-wrap: wrap;
 }
-.actions {
-  display: flex;
-  width: 90%;
-  justify-content: flex-end;
-  align-items: center;
-}
-#btnPost {
-  border-radius: 7px;
-  background-color: lightgrey;
-  color: grey;
-  box-shadow: none;
-  padding: 10px 0px;
-  width: 100%;
-  margin-top: 10px;
-  cursor: pointer;
-}
 
-#btnPost:hover {
-  opacity: 0.8;
-}
 </style>
