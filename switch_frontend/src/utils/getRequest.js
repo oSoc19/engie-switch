5d4c94127952009e3a06ed387d7cc89fa0f635f4
Axtux <axtux@hotmail.com>
2019-07-20 19:29:37 +0200

    clean

--- switch_frontend/src/utils/getRequest.js
+++
@@ -1,8 +0,0 @@
-let getRequest = (url) => {
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open("GET", url); // false for synchronous request
-    xmlHttp.send();
-    return xmlHttp.responseText;
-}
-
-export default getRequest;
