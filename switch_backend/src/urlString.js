b5027d91bb7ae89a9d55ed25dfee5b38b46c16c0
Axtux <axtux@hotmail.com>
2019-07-23 11:04:35 +0200

    svg image for profile

--- switch_backend/src/urlString.js
+++ switch_backend/src/urlString.js
@@ -7,9 +7,35 @@ function toBytes(urlString) {
   }
 }
 
+/*
+ * Takes buffer and return string
+ * Only detects png, jpeg and svg
+ * Return undefined if no image or unknown format
+ */
 function toUrlString(bytes) {
-  // TODO choose between png and jpeg
-  return 'data:' + 'image/png' + ';base64,' + bytes.toString('base64')
+  bytes = Buffer.from(bytes)
+  if(!bytes || bytes.length < 10) {
+    return;
+  }
+  let head = bytes.toString('hex', 0, 10)
+  let type;
+
+  if(head.substring(0, 4) == 'ffd8') {
+    type = 'image/jpeg';
+  }
+  if(head.substring(0, 16) == '89504e470d0a1a0a') {
+    type = 'image/png';
+  }
+  if(head.substring(0, 8) == '3c737667'
+    || head.substring(0, 10) == '3c3f786d6c'
+  ) {
+    type = 'image/svg+xml';
+  }
+  if(type) {
+    return 'data:' + type + ';base64,' + bytes.toString('base64')
+  } else {
+    console.log('unknown head: ', head)
+  }
 }
 
 module.exports = {
@@ -17,8 +43,12 @@ module.exports = {
   toUrlString,
 }
 
-//TEST
-//let content = require('fs').readFileSync('/home/axtux/Downloads/osoc/pics/5.png')
-//console.log(toUrlString(content));
+/* Test
+let content = require('fs').readFileSync('PATH_TO_IMAGE')
+console.log(toUrlString(content));
+//*/
 
-//require('fs').writeFileSync('test.png', toBytes(b64))
+/* Test
+let b64 = 'IMAGE_BASE_64'
+require('fs').writeFileSync('test.png', toBytes(b64))
+//*/
