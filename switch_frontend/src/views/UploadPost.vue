0e5cd35264d900ed13ccf50472a4d150253a0f50
Axtux <axtux@hotmail.com>
2019-07-25 00:46:08 +0200

    fix upload page

--- switch_frontend/src/views/UploadPost.vue
+++ switch_frontend/src/views/UploadPost.vue
@@ -1,13 +1,10 @@
 <template>
-  <div class="wrapper">
+  <div class="hello wrapper">
     <p class="upload__text">Upload your picture of the challenge</p>
     <upload-image :challengeId="challengeId" />
-    <div class="alert">
-      <div class="bad__image">X Bad Image</div>
-    </div>
     <ion-card class="challenge__card" v-if="challenge">
-      <img class="challenge__image" :src="challenge.image+'500'" />
-      <ion-card-header>
+      <div class="challenge__image" :style="{ backgroundImage: 'url(' + challenge.image + '500)' }"></div>
+      <ion-card-header class="challenge__card__header">
         <!--ion-card-subtitle>{{ challenge.title }}</ion-card-subtitle-->
         <ion-card-title>{{ challenge.title }}</ion-card-title>
       </ion-card-header>
@@ -41,53 +38,34 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-p {
-  text-align: center;
-}
 
 .upload__text {
+  text-align: center;
+  width: 100%;
   padding: 0;
   margin: 16px 0 0 0;
 }
 
-.yourChallenge {
-  color: var(--black);
-}
 .wrapper {
   width: 95%;
   display: flex;
   flex-wrap: wrap;
 }
 
-.alert {
-  display: flex;
-  width: 90%;
-  justify-content: flex-end;
-  align-items: center;
-  display: none;
-}
-.challenge__image {
-  max-width: 100%;
-  width: 100%;
-  max-height: 173px !important;
-  margin-top: 8px;
-  border-radius: 7px;
-  display: flex;
-  justify-content: center;
-  margin: 0 auto;
-}
-.bad__image {
-  width: 100%;
-  border: 2px solid var(--red);
-  color: var(--red);
-  margin: 5px 0;
-  text-align: center;
-  padding: 5px;
-  border-radius: 7px;
+.challenge__image{
+    background-size: cover;
+    background-position: center;
+    width: 100%;
+    height: 0;
+    padding-top: 75%;
 }
 
 .challenge__card{
   margin: 16px;
   padding: 0;
+}
+
+.challenge__card__header {
+  padding-bottom: 0;
 }
 </style>
